5dd2b1296509e25389049b1a1fd94a39079c3fa7
Prince <this.is.kumar.prince@gmail.com>
2022-02-09 20:53:37 +0530

    single linked list node completed

--- 1.TypeScript/dist/LinkedList/1.Singly_Linked_List.js
+++ 1.TypeScript/dist/LinkedList/1.Singly_Linked_List.js
@@ -1,3 +1,32 @@
 "use strict";
-console.log("hi");
-console.log("hello");
+class SNode {
+    _data;
+    _next;
+    set data(value) {
+        this._data = value;
+    }
+    get data() {
+        return this._data;
+    }
+    set next(value) {
+        this._next = value;
+    }
+    get next() {
+        return this._next;
+    }
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+class SLinkedList {
+    head;
+    constructor() {
+        this.head = null;
+    }
+    isEmpty() {
+        return this.head === null;
+    }
+}
+const list = new SLinkedList();
+console.log(list.isEmpty());
--- 1.TypeScript/src/LinkedList/1.Singly_Linked_List.ts
+++ 1.TypeScript/src/LinkedList/1.Singly_Linked_List.ts
@@ -1,2 +1,33 @@
-console.log("hi");
-console.log("hello");
+class SNode {
+  private _data!: number;
+  private _next!: SNode | null;
+  public set data(value: number) {
+    this._data = value;
+  }
+  public get data(): number {
+    return this._data;
+  }
+  public set next(value: SNode | null) {
+    this._next = value;
+  }
+  public get next(): SNode | null {
+    return this._next;
+  }
+  constructor(data: number, next: SNode | null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class SLinkedList {
+  private head!: SNode | null;
+  constructor() {
+    this.head = null;
+  }
+  public isEmpty() {
+    return this.head === null;
+  }
+}
+
+const list = new SLinkedList();
+console.log(list.isEmpty());
