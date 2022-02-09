bebbc817e4a0f804f55a38ff7d0711c75a4fd091
Prince <this.is.kumar.prince@gmail.com>
2022-02-09 22:48:23 +0530

    display methods added

--- 1.TypeScript/dist/LinkedList/1.Singly_Linked_List.js
+++ 1.TypeScript/dist/LinkedList/1.Singly_Linked_List.js
@@ -27,6 +27,79 @@ class SLinkedList {
     isEmpty() {
         return this.head === null;
     }
+    create(arr) {
+        arr.forEach((value) => {
+            this.add(value);
+        });
+    }
+    add(value) {
+        if (this.head === null) {
+            this.head = new SNode(value, null);
+        }
+        else {
+            let temp = this.head;
+            while (temp.next !== null) {
+                temp = temp.next;
+            }
+            temp.next = new SNode(value, null);
+        }
+    }
+    displayFromStart_ThroughLoop() {
+        let temp = this.head;
+        while (temp !== null) {
+            if (temp.next === null) {
+                process.stdout.write(`${temp.data}`);
+            }
+            else {
+                process.stdout.write(`${temp.data}, `);
+            }
+            temp = temp.next;
+        }
+        console.log();
+    }
+    displayFromStart_ThroughRecursion() {
+        this.displayFromStart_ThroughRecursion_RecursiveFunction(this.head);
+        console.log();
+    }
+    displayFromStart_ThroughRecursion_RecursiveFunction(curr) {
+        if (curr !== null) {
+            if (curr.next === null) {
+                process.stdout.write(`${curr.data}`);
+            }
+            else {
+                process.stdout.write(`${curr.data}, `);
+            }
+            this.displayFromStart_ThroughRecursion_RecursiveFunction(curr.next);
+        }
+    }
+    displayFromLast_ThroughRecursion() {
+        this.displayFromLast_ThroughRecursion_RecursiveFunction(this.head);
+        console.log();
+    }
+    displayFromLast_ThroughLoop() {
+        console.log();
+    }
+    displayFromLast_ThroughRecursion_RecursiveFunction(curr) {
+        if (curr !== null) {
+            this.displayFromLast_ThroughRecursion_RecursiveFunction(curr.next);
+            if (curr === this.head) {
+                process.stdout.write(`${curr.data}`);
+            }
+            else {
+                process.stdout.write(`${curr.data}, `);
+            }
+        }
+    }
 }
 const list = new SLinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.displayFromStart_ThroughLoop();
 console.log(list.isEmpty());
+list.displayFromStart_ThroughRecursion();
+console.log(list.isEmpty());
+list.create([5, 6, 7, 8, 9]);
+list.displayFromStart_ThroughLoop();
+list.displayFromLast_ThroughRecursion();
--- 1.TypeScript/src/LinkedList/1.Singly_Linked_List.ts
+++ 1.TypeScript/src/LinkedList/1.Singly_Linked_List.ts
@@ -1,33 +1,112 @@
-class SNode {
-  private _data!: number;
-  private _next!: SNode | null;
-  public set data(value: number) {
+class SNode<T> {
+  private _data!: T;
+  private _next!: SNode<T> | null;
+  public set data(value: T) {
     this._data = value;
   }
-  public get data(): number {
+  public get data(): T {
     return this._data;
   }
-  public set next(value: SNode | null) {
+  public set next(value: SNode<T> | null) {
     this._next = value;
   }
-  public get next(): SNode | null {
+  public get next(): SNode<T> | null {
     return this._next;
   }
-  constructor(data: number, next: SNode | null) {
+  constructor(data: T, next: SNode<T> | null) {
     this.data = data;
     this.next = next;
   }
 }
 
-class SLinkedList {
-  private head!: SNode | null;
+class SLinkedList<T> {
+  private head!: SNode<T> | null;
   constructor() {
     this.head = null;
   }
   public isEmpty() {
     return this.head === null;
   }
+  public create(arr: T[]) {
+    arr.forEach((value) => {
+      this.add(value);
+    });
+  }
+  public add(value: T) {
+    if (this.head === null) {
+      this.head = new SNode(value, null);
+    } else {
+      let temp = this.head;
+      while (temp.next !== null) {
+        temp = temp.next;
+      }
+      temp.next = new SNode(value, null);
+    }
+  }
+
+  public displayFromStart_ThroughLoop() {
+    let temp = this.head;
+    while (temp !== null) {
+      if (temp.next === null) {
+        process.stdout.write(`${temp.data}`);
+      } else {
+        process.stdout.write(`${temp.data}, `);
+      }
+      temp = temp.next;
+    }
+    console.log();
+  }
+
+  public displayFromStart_ThroughRecursion() {
+    this.displayFromStart_ThroughRecursion_RecursiveFunction(this.head);
+    console.log();
+  }
+
+  private displayFromStart_ThroughRecursion_RecursiveFunction(
+    curr: SNode<T> | null
+  ) {
+    if (curr !== null) {
+      if (curr.next === null) {
+        process.stdout.write(`${curr.data}`);
+      } else {
+        process.stdout.write(`${curr.data}, `);
+      }
+      this.displayFromStart_ThroughRecursion_RecursiveFunction(curr.next);
+    }
+  }
+
+  public displayFromLast_ThroughRecursion() {
+    this.displayFromLast_ThroughRecursion_RecursiveFunction(this.head);
+    console.log();
+  }
+
+  public displayFromLast_ThroughLoop() {
+    console.log();
+  }
+
+  private displayFromLast_ThroughRecursion_RecursiveFunction(
+    curr: SNode<T> | null
+  ) {
+    if (curr !== null) {
+      this.displayFromLast_ThroughRecursion_RecursiveFunction(curr.next);
+      if (curr === this.head) {
+        process.stdout.write(`${curr.data}`);
+      } else {
+        process.stdout.write(`${curr.data}, `);
+      }
+    }
+  }
 }
 
-const list = new SLinkedList();
+const list = new SLinkedList<number>();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.displayFromStart_ThroughLoop();
 console.log(list.isEmpty());
+list.displayFromStart_ThroughRecursion();
+console.log(list.isEmpty());
+list.create([5, 6, 7, 8, 9]);
+list.displayFromStart_ThroughLoop();
+list.displayFromLast_ThroughRecursion();
