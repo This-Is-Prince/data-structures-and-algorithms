e9abaaf2ffe52b144ead042395166831f6d04eb9
Prince <this.is.kumar.prince@gmail.com>
2022-02-24 06:00:34 +0530

    binary tree package created

--- go_dsa/main.go
+++ go_dsa/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"go_dsa/trees/binary"
+	"go_dsa/trees/binary_tree"
 )
 
 func main() {
 	fmt.Println("Data Structures and Algorithms")
 
-	binaryTree := binary.BinaryTree{}
+	binaryTree := binary_tree.BinaryTree{}
 	binaryTree.Create([]int{5, 8, 6, -1, 9, 3, -1, 4, 2}, -1)
 	binaryTree.PreOrderUsingRecursion(",")
 	binaryTree.PostOrderUsingRecursion(",")
--- go_dsa/trees/binary_tree/binary_tree.go
+++ go_dsa/trees/binary_tree/binary_tree.go
@@ -1,4 +1,4 @@
-package binary
+package binary_tree
 
 import (
 	"errors"
