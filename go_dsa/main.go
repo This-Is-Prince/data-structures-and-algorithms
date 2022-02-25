9eb3f3c22253699a5be107a943c33c66dd8a91b3
Prince <this.is.kumar.prince@gmail.com>
2022-02-25 15:59:39 +0530

    diff func created for debug diff datastructures

--- go_dsa/main.go
+++ go_dsa/main.go
@@ -2,55 +2,63 @@ package main
 
 import (
 	"fmt"
-	// "go_dsa/trees/binary_tree"
-	// questions "go_dsa/stack/questions/parenthesis_matching"
-	// "go_dsa/stack/using_array"
-	// "go_dsa/stack/using_linkedlist"
-	"go_dsa/queues/linear/using_array"
+
+	queue_using_array "go_dsa/queues/linear/using_array"
+	stk_questions "go_dsa/stack/questions/parenthesis_matching"
+	stk_using_array "go_dsa/stack/using_array"
+	stk_using_linkedlist "go_dsa/stack/using_linkedlist"
+	"go_dsa/trees/binary_tree"
 )
 
 func main() {
 	fmt.Println("Data Structures and Algorithms")
 
-	// binaryTree := binary_tree.BinaryTree{}
-	// binaryTree.Create([]int{5, 8, 6, -1, 9, 3, -1, 4, 2}, -1)
-	// binaryTree.PreOrderUsingRecursion(",")
-	// binaryTree.PostOrderUsingRecursion(",")
-	// binaryTree.InOrderUsingRecursion(",")
-	// binaryTree.InOrderUsingLoop(",")
-	// binaryTree.LevelOrderUsingLoop(",")
+	// debugStackUsingArray()
+	// debugStackUsingLinkedList()
+	// debugStackQuestions()
+	debugLinearQueueUsingArray()
+	// debugBinaryTree()
+}
 
-	// stk, err := using_array.Create(10)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	stk.Display(",")
-	// 	stk.Push(1)
-	// 	stk.Push(2)
-	// 	stk.Push(3)
-	// 	stk.Push(4)
-	// 	stk.Push(5)
-	// 	stk.Display(",")
-	// }
+func debugStackUsingArray() {
+	stk, err := stk_using_array.Create(10)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		stk.Display(",")
+		stk.Push(1)
+		stk.Push(2)
+		stk.Push(3)
+		stk.Push(4)
+		stk.Push(5)
+		stk.Display(",")
+	}
+}
 
-	// stk1 := using_linkedlist.Stack{}
-	// stk1.Display(",")
-	// stk1.Push(1)
-	// stk1.Push(2)
-	// stk1.Push(3)
-	// stk1.Push(4)
-	// stk1.Push(5)
-	// stk1.Display(",")
+func debugStackUsingLinkedList() {
 
-	// fmt.Println("Is '((a+b)*(c-d))' Balance", questions.IsBalance("((a+b)*(c-d))"))
-	// fmt.Println("Is '((a+b)*(c-d)' Balance", questions.IsBalance("((a+b)*(c-d)"))
-	// fmt.Println("Is '((a+b)*(c-d)))' Balance", questions.IsBalance("((a+b)*(c-d)))"))
-	// fmt.Println("Is '{([a+b]*[c-d])/e}' Balance", questions.IsBalance("{([a+b]*[c-d])/e}"))
-	// fmt.Println("Is '{([a+b]*[c-d]/e}' Balance", questions.IsBalance("{([a+b]*[c-d]/e}"))
-	// fmt.Println("Is '{([a+b]*[c-d)/e}' Balance", questions.IsBalance("{([a+b]*[c-d)/e}"))
-	// fmt.Println("Is '{([a+b)*[c-d]]/e}' Balance", questions.IsBalance("{([a+b)*[c-d]]/e}"))
+	stk1 := stk_using_linkedlist.Stack{}
+	stk1.Display(",")
+	stk1.Push(1)
+	stk1.Push(2)
+	stk1.Push(3)
+	stk1.Push(4)
+	stk1.Push(5)
+	stk1.Display(",")
+}
 
-	q, err := using_array.Create(5)
+func debugStackQuestions() {
+	fmt.Println("Is '((a+b)*(c-d))' Balance", stk_questions.IsBalance("((a+b)*(c-d))"))
+	fmt.Println("Is '((a+b)*(c-d)' Balance", stk_questions.IsBalance("((a+b)*(c-d)"))
+	fmt.Println("Is '((a+b)*(c-d)))' Balance", stk_questions.IsBalance("((a+b)*(c-d)))"))
+	fmt.Println("Is '{([a+b]*[c-d])/e}' Balance", stk_questions.IsBalance("{([a+b]*[c-d])/e}"))
+	fmt.Println("Is '{([a+b]*[c-d]/e}' Balance", stk_questions.IsBalance("{([a+b]*[c-d]/e}"))
+	fmt.Println("Is '{([a+b]*[c-d)/e}' Balance", stk_questions.IsBalance("{([a+b]*[c-d)/e}"))
+	fmt.Println("Is '{([a+b)*[c-d]]/e}' Balance", stk_questions.IsBalance("{([a+b)*[c-d]]/e}"))
+}
+
+func debugLinearQueueUsingArray() {
+	q, err := queue_using_array.Create(5)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -75,4 +83,14 @@ func main() {
 	q.Display(",")
 	fmt.Println(q.DeQueue())
 	q.Display(",")
+}
+
+func debugBinaryTree() {
+	binaryTree := binary_tree.BinaryTree{}
+	binaryTree.Create([]int{5, 8, 6, -1, 9, 3, -1, 4, 2}, -1)
+	binaryTree.PreOrderUsingRecursion(",")
+	binaryTree.PostOrderUsingRecursion(",")
+	binaryTree.InOrderUsingRecursion(",")
+	binaryTree.InOrderUsingLoop(",")
+	binaryTree.LevelOrderUsingLoop(",")
 }
