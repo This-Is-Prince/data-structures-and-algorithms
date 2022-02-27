bcb0097b8d797a79e4d2cdd0ce2f8eae908ae507
Prince <this.is.kumar.prince@gmail.com>
2022-02-27 06:03:25 +0530

    extra information added in bubble sort algo

--- go_dsa/sorting_techniques/bubble/sort.go
+++ go_dsa/sorting_techniques/bubble/sort.go
@@ -1,5 +1,22 @@
 package bubble
 
+/*
+	Critera for Analysis
+	1. Number of Comparisons
+	2. Number of Swaps
+	3. Adaptive (if complexity changes based on pre-sorted input i.e. pre-sorted array of the input affects the running time.)
+	4. Stable (Sorting algorithm is stable if two elements with equal values appear in the same order in output as it was in the input.)
+	5. Extra Memory
+
+	1. min - O(n), max - O(n^2)
+	2. min - O(1), max - O(n^2)
+	3. we make bubble sort adaptive
+	4. bubble sort is stable
+	5. no extra memory required
+
+	TIME
+	min - O(n), max - O(n^2)
+*/
 func Sort(values []int) {
 	n, flag := len(values), false
 	for i := 0; i < n-1; i++ {
